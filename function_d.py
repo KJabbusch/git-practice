b075e4cbdd0c1f0f22b9d442259191bb0d310e00
goblinbrooke <BrookeVlahos@gmail.com>
2021-09-17 15:08:37 -0700

    an ode to Git

--- function_d.py
+++ function_d.py
@@ -2,7 +2,12 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    return max_value([1, 12, 2, 42, 8, 3])
+
+    # An ode to Git: 
+    # Do we Git it? 
+    # Not sure
+    # One day I hope to Get with it
 
 
 if __name__ == "__main__":
